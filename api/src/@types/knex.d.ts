87345676d9768cbdd6bb19518a773bda2b30864d
Richard Freitas <wesleyrichard30@gmail.com>
2024-01-29 07:41:08 -0300

    chore: create migration types

---
+++ api/src/@types/knex.d.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+import { Knex } from 'knex';
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string;
+      name: string;
+      email: number;
+      session_id?: string;
+    };
+    meals: {
+      id: string;
+      user_id: string;
+      name: string;
+      description: string;
+      is_diet: boolean;
+      date: Date;
+    };
+  }
+}
