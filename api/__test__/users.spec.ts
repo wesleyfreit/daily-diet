64b6c35727ea06a989ebbb881fe5ad971e895acd
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-02 21:45:30 -0300

    test: add tests to get meal metrics from a user

--- api/__test__/users.spec.ts
+++ api/__test__/users.spec.ts
@@ -3,12 +3,8 @@ import request from 'supertest';
 import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest';
 
 import { app } from '../src/app';
-
-const user = {
-  name: 'Richard',
-  email: 'richard@gmail.com',
-  password: '123456',
-};
+import { meals } from './mocks/meals';
+import { users } from './mocks/users';
 
 describe('User routes', () => {
   beforeAll(async () => {
@@ -30,7 +26,7 @@ describe('User routes', () => {
   it('should be able to create a user', async () => {
     const createUserResponse = await request(app.server)
       .post('/signup')
-      .send(user)
+      .send(users[0])
       .expect(201);
 
     expect(createUserResponse.body).toEqual(
@@ -42,17 +38,62 @@ describe('User routes', () => {
   });
 
   it('should be able to authenticate as a user', async () => {
-    await request(app.server).post('/signup').send(user).expect(201);
+    await request(app.server).post('/signup').send(users[0]).expect(201);
 
     const loginResponse = await request(app.server)
       .post('/signin')
-      .send(user)
+      .send(users[0])
       .expect(200);
 
     expect(loginResponse.body).toEqual(
       expect.objectContaining({
         user_id: expect.stringMatching(/\S/),
         token: expect.stringMatching(/\S/),
+      }),
+    );
+  });
+
+  it('should be able to get meals metrics from a user', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/signup')
+      .send(users[0])
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${createUserResponse.body.token}`)
+      .send(meals[0])
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${createUserResponse.body.token}`)
+      .send(meals[1])
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${createUserResponse.body.token}`)
+      .send(meals[2])
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${createUserResponse.body.token}`)
+      .send(meals[3])
+      .expect(201);
+
+    const getMetricsResponse = await request(app.server)
+      .get('/users/metrics')
+      .set('Authorization', `Bearer ${createUserResponse.body.token}`)
+      .expect(200);
+
+    expect(getMetricsResponse.body).toEqual(
+      expect.objectContaining({
+        totalMeans: 4,
+        totalMealsOnDiet: 3,
+        totalMealsOffDiet: 1,
+        bestOnDietSequence: 2,
       }),
     );
   });
